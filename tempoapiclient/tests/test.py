a77b396473d6980401f81396c05e8f8e34b7559e
Stanislav Ulrych <stanislav.ulrych@gmail.com>
2019-12-30 15:28:38 +0100

    Added setup.cfg and tests.

---
+++ tempoapiclient/tests/test.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+import tempoapiclient
+
+class TestModule(TestCase):
+    def test_client_creation(self):
+        from tempoapiclient import client
+
+        tempo = tempoapiclient.client.Tempo(
+            auth_token="<your_tempo_api_key>",
+            base_url="https://api.tempo.io/core/3")
+
+        self.assertTrue(isinstance(tempo, tempoapiclient.Client.Tempo))
