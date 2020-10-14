08155741689dcd4d786494714ca3dd98a460b788
Stanislav Ulrych <stanislav.ulrych@gmail.com>
2020-10-14 09:14:28 +0200

    Making version 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tempo-api-python-client",
-    version="0.3.0",
+    version="0.4.0",
     author="Stanislav Ulrych",
     author_email="stanislav.ulrych@gmail.com",
     description="Python bindings for Tempo (https://tempo-io.github.io/tempo-api-docs/)",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6.9',
 )
