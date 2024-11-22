f2b658f72b4af4ec029ca4495d0927a9e393ca49
DataGrow-admin <118192837+DataGrow-admin@users.noreply.github.com>
2024-11-22 12:08:58 +0100

    Update client_v4.py

--- tempoapiclient/client_v4.py
+++ tempoapiclient/client_v4.py
@@ -488,6 +488,16 @@ class Tempo(RestAPIClient):
 
         return self.get(url, params=params)
 
+    def get_floating_holidays(self, holidaySchemeId=None):
+        """
+        Retrieve floating holidays for an existing holiday scheme.
+        :param holidaySchemeId:
+        """
+        url = f"/holiday-schemes/{holidaySchemeId}/holidays/floating"
+
+        return self.get(url, params=params)
+
+    
     def create_holiday_scheme(self, schemeName, schemeDescription=None):
         """
         Create holiday scheme
